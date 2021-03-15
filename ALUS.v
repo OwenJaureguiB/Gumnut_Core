c3ca4980d4b9e420582114210e7146a19b76c4a1
OwenJaureguiB <71340202+OwenJaureguiB@users.noreply.github.com>
2021-03-14 22:30:50 -0600

    Arreglado problema con shifts

--- ALUS.v
+++ ALUS.v
@@ -10,8 +10,8 @@ module ALUS (
 		case(ALUOp_i)
 			2'b00: {cout, res} = {1'b0, rs_i} << count_i;
 			2'b01: {res, cout} = {rs_i, 1'b0} >> count_i;
-			2'b10: {res, cout} = {rs_i << count_i | rs_i >> 4'h8-count_i, 1'b0};
-			2'b11: {res, cout} = {rs_i >> count_i | rs_i << 4'h8-count_i, 1'b0};
+			2'b10: {cout, res} = {1'b0, rs_i << count_i} | {rs_i >> 4'h8-count_i};
+			2'b11: {res, cout} = {rs_i >> count_i, 1'b0} | {rs_i << 4'h8-count_i, 1'b0};
 			default: {res, cout} = {8'hxx, 1'bx};
 		endcase
 	end
